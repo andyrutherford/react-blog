308e95e8fc83d0978e524b4c243c87c31c6d0aa6
Andy <andrew.rutherford.6@gmail.com>
2018-09-24 10:51:00 +0300

    redirect to posts page after new post submit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,55 +1,44 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-
-    render () {   
+    render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                                to="/" 
+                            <li><NavLink
+                                to="/posts/"
                                 exact
+                                activeClassName="my-active"
                                 activeStyle={{
-                                    color: 'green',
+                                    color: '#fa923f',
                                     textDecoration: 'underline'
-                                }}>Posts</NavLink>
-                            </li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
-                                hash:'#submit',
+                                hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</NavLink>
-                            </li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route 
-                        path="/" 
-                        exact
-                        component={Posts} />
-                    <Route 
-                        path="/new-post" 
-                        exact
-                        component={NewPost} />
-                    <Route 
-                        path="/:id" 
-                        exact
-                        component={FullPost} />
-                </Switch>
-
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                </Switch> 
             </div>
         );
     }
 }
 
-export default Blog;
+export default Blog;  
