0f1c6156b1e0920ae07c98c33378ff7c15d8c593
Andy <andrew.rutherford.6@gmail.com>
2018-09-24 10:58:31 +0300

    added 404 routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,11 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -32,9 +37,11 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Page not found!</h1>} />
+                    { /*    <Redirect from="/" to="/posts" /> */}
+                    { /*    <Route path="/" component={Posts} /> */ }
                 </Switch> 
             </div>
         );
